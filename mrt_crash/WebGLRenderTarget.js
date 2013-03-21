77e92bc2da1d68c9cc964c4dd6d18cb9b1ae132f
Aleksandar Rodic <aleksandar.xyz@gmail.com>
2013-03-21 14:32:19 -0700

    added tests

---
+++ mrt_crash/WebGLRenderTarget.js
@@ -0,0 +1,105 @@
+/**
+ * @author szimek / https://github.com/szimek/
+ * @author alteredq / http://alteredqualia.com/
+ */
+THREE.WebGLRenderTargetArray = function( width, height, length, bindDepth ){
+	
+	THREE.EventDispatcher.call( this );
+
+	this.generateMipmaps = true;
+	this.width = width;
+	this.height = height;
+	this.color = [];
+
+	for (var i = 0; i < length; ++i) {
+			this.color.push(new THREE.WebGLRenderTarget(width, height));
+	}
+
+	if (bindDepth) {
+
+		this.depth = new THREE.WebGLRenderTarget(width, height, {
+			magFilter: THREE.LinearFilter,
+			minFilter: THREE.LinearFilter// TODO LinearMipMapLinearFilter crshes webkit
+		});
+	
+	}
+
+}
+
+THREE.WebGLRenderTargetArray.prototype = Object.create( Array.prototype );
+
+THREE.WebGLRenderTarget.prototype.dispose = function () {
+
+	for (var i in this){
+		this[i].dispatchEvent( { type: 'dispose' } );
+	}
+
+
+};
+
+THREE.WebGLRenderTarget = function ( width, height, options ) {
+
+	THREE.EventDispatcher.call( this );
+
+	this.width = width;
+	this.height = height;
+
+	options = options || {};
+
+	this.wrapS = options.wrapS !== undefined ? options.wrapS : THREE.ClampToEdgeWrapping;
+	this.wrapT = options.wrapT !== undefined ? options.wrapT : THREE.ClampToEdgeWrapping;
+
+	this.magFilter = options.magFilter !== undefined ? options.magFilter : THREE.LinearFilter;
+	this.minFilter = options.minFilter !== undefined ? options.minFilter : THREE.LinearMipMapLinearFilter;
+
+	this.anisotropy = options.anisotropy !== undefined ? options.anisotropy : 1;
+
+	this.offset = new THREE.Vector2( 0, 0 );
+	this.repeat = new THREE.Vector2( 1, 1 );
+
+	this.format = options.format !== undefined ? options.format : THREE.RGBAFormat;
+	this.type = options.type !== undefined ? options.type : THREE.UnsignedByteType;
+
+	this.depthBuffer = options.depthBuffer !== undefined ? options.depthBuffer : true;
+	this.stencilBuffer = options.stencilBuffer !== undefined ? options.stencilBuffer : true;
+
+	this.generateMipmaps = true;
+
+	this.shareDepthFrom = null;
+
+};
+
+THREE.WebGLRenderTarget.prototype.clone = function() {
+
+	var tmp = new THREE.WebGLRenderTarget( this.width, this.height );
+
+	tmp.wrapS = this.wrapS;
+	tmp.wrapT = this.wrapT;
+
+	tmp.magFilter = this.magFilter;
+	tmp.minFilter = this.minFilter;
+
+	tmp.anisotropy = this.anisotropy;
+
+	tmp.offset.copy( this.offset );
+	tmp.repeat.copy( this.repeat );
+
+	tmp.format = this.format;
+	tmp.type = this.type;
+
+	tmp.depthBuffer = this.depthBuffer;
+	tmp.stencilBuffer = this.stencilBuffer;
+
+	tmp.generateMipmaps = this.generateMipmaps;
+
+	tmp.shareDepthFrom = this.shareDepthFrom;
+
+	return tmp;
+
+};
+
+THREE.WebGLRenderTarget.prototype.dispose = function () {
+
+	this.dispatchEvent( { type: 'dispose' } );
+
+};
